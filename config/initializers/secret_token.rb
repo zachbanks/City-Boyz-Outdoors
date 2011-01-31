04f04a2f6c4bdae66e45f264514ac91b89301286
Zach Banks <zachbanks@mac.com>
2011-01-30 19:01:36 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CityBoyzOutdoors::Application.config.secret_token = '72304458722abb8f2c8111453564692e8790203b1fd1b5118b10e930fb040885165ee3a3cdf76bb4bfef1ad3ca9bfc95fa481b2528ee345ca12fa8be525f4050'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+CityBoyzOutdoors::Application.config.session_store :cookie_store, :key => '_city_boyz_outdoors_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# CityBoyzOutdoors::Application.config.session_store :active_record_store
