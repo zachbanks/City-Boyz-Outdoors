8cfdfa41dc5b23e726f8caf41a02a24c27f4d82d
Zach Banks <zachbanks@mac.com>
2011-01-31 18:40:50 -0500

    Added a logo image to the project.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   # Returns the logo for the website.
   def logo
-    image_tag("logo.png", :alt => "City Boyz Outdoors")
+    image_tag("logo.jpg", :alt => "City Boyz Outdoors", :size => "300x150")
   end
 end
