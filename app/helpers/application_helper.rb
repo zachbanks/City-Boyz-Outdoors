2a6846d28a7e92efa8e89db3ffa2e47d6024ea7a
Zach Banks <zachbanks@mac.com>
2011-01-31 22:30:16 -0500

    Added first set of RSpec tests to pages_controller_spec.rb testing that each page has a correct title. Implemented machinery to make thoses tests past.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,14 @@
 module ApplicationHelper
-  # # Returns the title for a given page.
-  # def title
-  #   
-  # end
+  # Returns the title for a given page.
+  def title
+    base_title = "City Boyz Outdoors"
+    unless @title
+      base_title
+    else
+      "#{base_title} | #@title"
+    end
+  end
+  
   # Returns the logo for the website.
   def logo
     image_tag("logo.jpg", :alt => "City Boyz Outdoors", :size => "300x150")
