253d74b2906c92ce42358d324de422cf96247965
Zach Banks <zachbanks@me.com>
2011-02-16 06:56:52 -0500

    Removed setup_mailer.rb from git.

--- config/initializers/setup_mailer.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => "gmail.com",
-  :user_name            => "cityboyzoutdoors@gmail.com",
-  :password             => "codyisqueer",
-  :authentication       => "plain",
-  :enable_starttls_auto => true
-}
