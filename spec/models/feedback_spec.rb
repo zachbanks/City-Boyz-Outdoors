a0f27153843629e7a9d79df52fa38ff6c606c0fc
Zach Banks <zachbanks@me.com>
2011-02-16 22:02:19 -0500

    Updated feedback model specs.

--- spec/models/feedback_spec.rb
+++ spec/models/feedback_spec.rb
@@ -7,11 +7,20 @@ describe Feedback do
   
   describe "validations" do
     context "success" do
-      it "should allow valid emails"
+      it "should allow valid emails" do
+        feedback = Feedback.new(@attr)
+        feedback.save
+        feedback.errors.empty?.should be_true
+      end
     end
     
     context "failure" do
-      it "should give an error for invaild emails"
+      it "should give an error for invaild emails" do
+        feedback = Feedback.new
+        feedback.save
+        feedback.errors.any?.should be_true
+        feedback.errors[:email][1].should =~ /email is invalid/ # Crude, but it works.
+      end
     end
   end
 end
