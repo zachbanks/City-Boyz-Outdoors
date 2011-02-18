8752634480f1a45426d7ff3a0d7728f5cd15f4f5
Zach Banks <zachbanks@me.com>
2011-02-17 20:07:54 -0500

    Added Twiiter and Facebook icons to homepage so users can follow those respective groups.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,6 +11,17 @@ module ApplicationHelper
   
   # Returns the logo for the website.
   def logo
-    image_tag("logo.jpg", :alt => "City Boyz Outdoors", :size => "300x150")
+    image_tag("logo.png", :alt => "City Boyz Outdoors", :size => "300x150")
   end
+  
+  # Returns the Facebook logo.
+  def facebook_logo
+    image_tag("icons/facebook_logo.png", :alt => "Check us out on Facebook!", :size => "50x50")
+  end
+  
+  # Returns the Twitter logo.
+  def twitter_logo
+    image_tag("icons/twitter_logo.png", :alt => "Follow us on Twitter!", :size => "50x50")
+  end
+  
 end
