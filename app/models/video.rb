e57877e278ad74a50c81ac1c5fbee0d82c3a1a19
Zach Banks <zachbanks@me.com>
2011-02-28 00:17:13 -0500

    Updated seeds.rb to create video objects., and added message to one of the Video's validations.

--- app/models/video.rb
+++ app/models/video.rb
@@ -3,9 +3,9 @@ class Video < ActiveRecord::Base
   
   validates :title, :description, :presence => true
   validates :source_url, 
-            :presence => true, 
+            :presence => { :message => "Source url cannot be blank and must be a valid url." },
             :format => { :with => /http:\/\/.*/ }
-            # :message => "Source url cannot be blank and must be a valid url." # TODO Make regex better.
+             # TODO Make regex better.
   
   default_scope :order => 'videos.created_at DESC'
   
