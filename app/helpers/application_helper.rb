3014ed5d54562c1f09644d51217bf1954f3c8df8
Zach Banks <zachbanks@me.com>
2011-03-28 22:29:24 -0400

    Cleaned up disqus code.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -25,25 +25,23 @@ module ApplicationHelper
   end
   
   def disqus_thread
-    %Q{
-      <div id="disqus_thread"></div>
-    		<script type="text/javascript">
-    		    /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
-    		    var disqus_shortname = 'cityboyzoutdoors'; // required: replace example with your forum shortname
-            var disqus_developer = 1;
-    		    // The following are highly recommended additional parameters. Remove the slashes in front to use.
-    		    var disqus_identifier = 'cityboyzoutdoors';
-    		    // var disqus_url = 'http://example.com/permalink-to-page.html';
+    %{
+    <div id="disqus_thread">
+      <script type="text/javascript">
+    	    var disqus_shortname = 'cityboyzoutdoors';
+          var disqus_developer = 1;
+    		  //var disqus_identifier = 'cityboyzoutdoors21724';
+    		  //var disqus_url = '#{request.url}'; // Rough, but works for now.
 
-    		    /* * * DON'T EDIT BELOW THIS LINE * * */
-    		    (function() {
-    		        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-    		        dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
-    		        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-    		    })();
-    		</script>
-    		<noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
-    		<a href="http://disqus.com" class="dsq-brlink">blog comments powered by <span class="logo-disqus">Disqus</span></a>
+    		  /* * * DON'T EDIT BELOW THIS LINE * * */
+    		  (function() {
+    		      var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+    		      dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+    		      (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    		  })();
+    	</script>
+    	<noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+    </div>
     }
   end
   
