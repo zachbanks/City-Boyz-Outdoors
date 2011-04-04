8eb966f40e640736ab1221a5f19d7c827beb0182
Zach Banks <zachbanks@me.com>
2011-04-03 21:22:24 -0400

    Finished CRUD for TeamMembersController.

--- app/controllers/team_members_controller.rb
+++ app/controllers/team_members_controller.rb
@@ -12,4 +12,31 @@ class TeamMembersController < ApplicationController
     @title = "New Team Member"
   end
   
+  def create
+    @title = "New Team Member"
+    if team_member.save
+      redirect_to team_members_path, :notice => "#{team_member.name} added as a team member."
+    else
+      render :action => :new
+    end
+  end
+  
+  def edit
+    @title = "Editing #{team_member.name}"
+  end
+  
+  def update
+    if team_member.update_attributes(params[:team_member])
+      redirect_to team_members_path, :notice => "#{team_member.name} was succesfully updated."
+    else
+      render :action => :edit
+    end
+  end
+  
+  def destroy
+    msg = "#{team_member.name} was succesfully deleted."
+    team_member.destroy
+    redirect_to team_members_path, :flash => { :success => msg }
+  end
+  
 end
