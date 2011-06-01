7f275de99774c0dc236ce20081faabe81e0874c4
Zach Banks <zachbanks@me.com>
2011-05-31 22:43:46 -0400

    Added Twitter.rb

---
+++ config/initializers/twitter.rb
@@ -0,0 +1,6 @@
+Twitter.configure do |config|
+  config.consumer_key = "zMFOxDTIPPggrgFCQGEQ"
+  config.consumer_secret = "qvry9Z4yp7nbw0hhL3aA8iTDha2lXUGlcv2hZsps"
+  config.oauth_token = "253828991-4Dh39l5oTrTid7qqb21Vtc1BLc8tfKElTPJnE3e2"
+  config.oauth_token_secret = "oXmI9bmupoPkGanradb2bDL4kQB5Pov8caqyD0mhs"
+end
