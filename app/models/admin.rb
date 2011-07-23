15528fb3326d0fc3d0a64ec340a6e76f2013c088
Zach Banks <zachbanks@me.com>
2011-07-23 14:41:27 -0400

    Fixed Forgot Password bug.

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -8,7 +8,7 @@ class Admin < ActiveRecord::Base
 
   validates_uniqueness_of :email, :allow_blank => true
   validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
-  validates_presence_of :password
+  validates_presence_of :password, :on => :create
   validates_confirmation_of :password
   validates_length_of :password, :minimum => 4, :allow_blank => true
 
@@ -25,7 +25,7 @@ class Admin < ActiveRecord::Base
   # Remember Me.
   def generate_token(column)
     begin
-      self[column] = SecureRandom.base64(10)
+      self[column] = SecureRandom.urlsafe_base64
     end while Admin.exists?(column => self[column])
   end
 
